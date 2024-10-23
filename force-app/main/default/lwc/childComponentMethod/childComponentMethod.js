f7ee1aa824d029f3cc1e14c021069e9d02aa20c7
Nagaraju <naga61648@gmail.com>
2024-10-23 11:44:08 +0530

    Initial commit

--- force-app/main/default/lwc/childComponentMethod/childComponentMethod.js
+++ force-app/main/default/lwc/childComponentMethod/childComponentMethod.js
@@ -3,6 +3,7 @@ import { LightningElement } from 'lwc';
 export default class childComponentMethod extends LightningElement {
     // Method to be called from the parent component
     performAction() {
+        alert('Action performed in child component!');
         console.log('Action performed in child component!');
         // Add your processing logic here
     }
--- force-app/main/default/lwc/parentComponentProcess/parentComponentProcess.js
+++ force-app/main/default/lwc/parentComponentProcess/parentComponentProcess.js
@@ -3,6 +3,7 @@ import { LightningElement } from 'lwc';
 export default class ParentComponentProcess extends LightningElement {
     // Method to call the child component's method
     callChildMethod() {
+        alert('Calling Child Method!');
         const child = this.template.querySelector('c-child-component-method');
         if (child) {
             child.performAction(); // Call the method in the child component
